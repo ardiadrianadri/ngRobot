46977a7fd12b1110880b14093649b17f4b9778fd
Victor Ocariz <damzeloca@gmail.com>
2016-05-15 11:47:30 +0200

    SelectBox atom semi-developed

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,11 @@
-import {Component} from "@angular/core";
+import {Component,OnInit} from "@angular/core";
 import {OrderComponent} from "./atoms/order/order.component";
+import {SwitchObject} from "./atoms/selectBox/switchObject";
+import {SelectBoxComponent} from "./atoms/selectBox/selectBox.component";
 
 @Component({
     selector: "my-app",
-    directives: [OrderComponent],
+    directives: [OrderComponent,SelectBoxComponent],
     template: `
 <StackLayout>
     <Label text="Tap the button" class="title"></Label>
@@ -12,11 +14,13 @@ import {OrderComponent} from "./atoms/order/order.component";
 
     <Label [text]="message" class="message" textWrap="true"></Label>
     <ng-robot-order value="up"></ng-robot-order>
+    <ng-robot-select-box [selectionlist]="ifSelectBoxList"></ng-robot-select-box>
 </StackLayout>
 `,
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     public counter: number = 16;
+    private ifSelectBoxList:Array<SwitchObject> = [];
 
     public get message(): string {
         if (this.counter > 0) {
@@ -28,5 +32,13 @@ export class AppComponent {
     
     public onTap() {
         this.counter--;
+    }
+
+    ngOnInit(){
+        let obj = new SwitchObject("obstacle","If obstacle");
+        let obj2 = new SwitchObject("notObstacle","Not obstacle");
+        this.ifSelectBoxList.push(obj);
+        this.ifSelectBoxList.push(obj2);
+        console.log(JSON.stringify(this.ifSelectBoxList[0]));
     }
 }
